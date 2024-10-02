0b734941417b08b71b93683131bac8c18af9107e
Arthur de Paula Correa <arthurdepaulacorrea@hotmail.com>
2024-10-02 12:43:27 -0300

    Configure: PostgreSQL Database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
-// server.js
 const express = require('express');
 const dotenv = require('dotenv');
+const { Sequelize } = require('sequelize');
 
 // Carregar variáveis de ambiente do arquivo .env
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// Configurando o Sequelize
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
+
+// Testando a conexão com o banco de dados
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  })
+  .catch(err => {
+    console.error('Erro ao conectar no banco de dados:', err);
+  });
 
 // Middleware para interpretar JSON
 app.use(express.json());
